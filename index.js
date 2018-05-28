ada1933b350f3d48669a7fb590e663c840d9b17d
Sergio <zsoftz@gmail.com>
2018-05-28 17:26:38 -0600

    test

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-var app = express()
+var app = express();
   //.use(express.static(path.join(__dirname, 'public')))
   //.set('views', path.join(__dirname, 'views'))
   //.set('view engine', 'ejs')
   //.get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
 app.get("/api", function(req, res) {
 	res.status(200);
