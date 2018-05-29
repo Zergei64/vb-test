cc5fbb90409d6e30235bcadeac0c5810662019e0
Sergio <zsoftz@gmail.com>
2018-05-28 22:47:05 -0600

    test

--- index.js
+++ index.js
@@ -13,14 +13,29 @@ var app = express();
 //app.use(express.logger('dev'));     /* 'default', 'short', 'tiny', 'dev' */
 app.use(bodyParser.json());
 
+// Add headers
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'https://votacall.na35.visual.force.com');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
 
 app.get("/api", function(req, res) {
 	res.status(200).send("Test");
 });
 
-app.route("/api").get( function(req, res) {
-	res.status(200).send("Test2"); 
-});
-  
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
