e4814d3760a4cafc8b228fbb651c54bceed51f80
Sergio <zsoftz@gmail.com>
2018-05-28 22:38:41 -0600

    test

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ var app = express();
   //.set('view engine', 'ejs')
   //.get('/', (req, res) => res.render('pages/index'))
 
-app.configure(function () {
-	app.use(express.logger('dev'));     /* 'default', 'short', 'tiny', 'dev' */
-	app.use(express.bodyParser());
-});
+
+app.use(express.logger('dev'));     /* 'default', 'short', 'tiny', 'dev' */
+app.use(express.bodyParser());
+
 
 app.get("/api", function(req, res) {
 	res.status(200).send("Test");
